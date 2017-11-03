04888c15911991742947fa8dd5d419e88ef62f24
champiiy <albert1047@hotmail.com>
2017-11-03 10:21:14 -0600

    UPDATE ENVIA V1.1

--- vistas/envia.php
+++ vistas/envia.php
@@ -22,8 +22,7 @@ $mail->From = $correo;
 $mail->FromName = $nombre;
 
 //To address and name
-// $mail->addAddress("info@transportemevisa.com");
-$mail->addAddress("ballina.santiago@gmail.com");
+$mail->addAddress("info@transportemevisa.com");
 
 
 $mail->isHTML(true);
